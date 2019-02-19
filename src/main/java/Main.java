056d19c78d5ce4a4d3b8595777277cf3c5128959
kooler.kilevpc@gmail.com <kooler.kilevpc@gmail.com>
2019-02-20 03:11:28 +0700

    push first project: mergesorting : version update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,37 +11,99 @@ import static java.util.Arrays.copyOf;
 public class Main {
 
     public static void main(String[] args) {
+
+        //PARSING-----------------------------------------
+
         List<String> cmdLine = Arrays.asList(args);
-        boolean reverseSort =cmdLine.contains("-d");
+        boolean reverseSort = false;
+        boolean intType; // data type
+        int startParsing;// marker to start to read in[1...n].txt files
+        int secondKeyMarker = 1;
+        String outPathName;
+
+        if(cmdLine.size() <= 3){// checking minimal number of args
+            System.out.println("Too few args to start: " + cmdLine.size());
+            System.exit(1);
+        }
+
+        if(cmdLine.get(0).equals("-a")){
+            reverseSort = false;
+            outPathName = cmdLine.get(2);
+            startParsing = 3;
+        }
+        else if(cmdLine.get(0).equals("-d")) {
+            reverseSort = true;
+            outPathName = cmdLine.get(2);
+            startParsing = 3;
+        } else {
+            outPathName = cmdLine.get(1);
+            startParsing = 2;
+        }
+
+        if (startParsing == 3)
+            secondKeyMarker = 1;
+        else
+            secondKeyMarker = 0;
+        if(cmdLine.get(secondKeyMarker).equals("-i"))
+            intType = true;
+        else if(cmdLine.get(secondKeyMarker).equals("-s"))
+            intType = false;
+        else {
+            System.out.println("wrong key");
+            System.exit(1);
+        }
 
 
-//        Options options = new Options();
-//        options.addOption(new Option("a", "asc", false, "Sort order: ASCending"));
-//        options.addOption(new Option("d", "desc", false, "Sort order: DESCending"));
-//        options.addOption(new Option("s", "stringSort", false, "Sorting datatype: Strings"));
-//        options.addOption(new Option("i", "integerSort", false, "Sorting datatype: Integers"));
-//
-//        CommandLine line = null;
-//        final CommandLineParser parser = new DefaultParser();
+
+
+
+        for (int i = startParsing; i < cmdLine.size(); i++){
+
+
+        }
+        //PARSING-----------------------------------------
+
+        //READING-----------------------------------------
 //        try {
-//            line = parser.parse(options, args);
-//            System.out.println("Sort order ASC: " + line.hasOption("a"));
-//            System.out.println("Sort order DESC: " + line.hasOption("d"));
-//            if(line.getArgs().length < 2) {
-//                throw new ParseException("Too few files to processing: " + line.getArgs().length);
+//            String pathName1 = "in1.txt";
+//            File file = new File(pathName1);
+//            FileReader fileReader = new FileReader(file); // connect to input file
+//            BufferedReader bufferedReader = new BufferedReader(fileReader); // connect FileReader with BufferedReader
+//
+//            String line;
+//            while((line = bufferedReader.readLine()) != null) {
+//                int in1Array; // выводим содержимое файла на экран построчно
 //            }
-//        } catch (ParseException e) {
-//            System.err.println("Parsing failed. Reason: " + e.getMessage());
-//            System.exit(1);
+//
+//            bufferedReader.close(); // закрываем поток
+//        } catch (Exception e) {
+//            e.printStackTrace();
 //        }
+        //READING-----------------------------------------
 
-        int[] inputMas = new int[]{1, 4, 2, 6, 4, 7, 4, 2, 5, 7, 4, 8, 1, 0, 100, 123, 423, 123, 432, 523, 43, 2, 34, 23, 4, 8};
 
-        lounchSort(inputMas, false);
+        int[] inputMas = new int[]{1, 4, 2, 6, 4, 7, 4, 2, 5, 7, 4, 8, 1, 0, 100, 123, 423, 123, 432, 523, 43, 2, 34, 23, 4, 8};//test Array
+
+        lounchSort(inputMas, reverseSort);
         System.out.println("sort Array");
-        for (   int i = 0;
-                i < inputMas.length; i++)
-            System.out.print(inputMas[i] + " ");
+        System.out.println("\t" + Arrays.toString(inputMas));
+
+        //WRITING-----------------------------------------
+        try{
+            File outputFile = new File(outPathName);
+
+            if(!outputFile.exists())
+                outputFile.createNewFile();
+            PrintWriter pw = new PrintWriter(outputFile);
+            for(int i = 0; i<inputMas.length; i++)
+                pw.println(inputMas[i]);
+            pw.close();
+
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e);
+        }
+
+        //WRITING-----------------------------------------
 
         /* args parsing and file loading
         if(file.isExists()) {
@@ -63,7 +130,7 @@ public class Main {
     }
 
     /**
-     * Sort Array
+     * Sort single Array (if he is unsorted)
      * @param a Array to sort
      * @param sortReverse boolean for reverse sorting order (false = asc, true = desc)
      * @return sorted array
@@ -73,46 +140,61 @@ public class Main {
         int left = 0;
         int right = a.length - 1;
 
-        sort(a, left, right, sortReverse);
+        sortSingleMas(a, left, right, sortReverse);
 
         return a;
     }
 
     /**
-     *
+     * recursive function sorting
      * @param a Array to sort
      * @param left left limit
      * @param right right limit
      * @param sortReverse method of sorting
      */
-    private static void sort(int[] a, int left, int right, boolean sortReverse) {
+    private static void sortSingleMas(int[] a, int left, int right, boolean sortReverse) {
 
         if (right <= left)
             return;
 
         int mid = left + (right - left) / 2;
-        sort(a, left, mid, sortReverse);
-        sort(a, mid + 1, right, sortReverse);
+        sortSingleMas(a, left, mid, sortReverse);
+        sortSingleMas(a, mid + 1, right, sortReverse);
 
-        int[] buf = copyOf(a, a.length);
+        int[] buffer = copyOf(a, a.length);
 
         int i = left, j = mid + 1;
         for (int m = left; m <= right; m++) {
 
             if (i > mid) {
-                a[m] = buf[j];
+                a[m] = buffer[j];
                 j++;
             } else if (j > right) {
-                a[m] = buf[i];
+                a[m] = buffer[i];
                 i++;
-            } else if (buf[j] < buf[i]) {
-                a[m] = buf[j];
-                j++;
+            //} else if (buffer[j] < buffer[i]) {
+            } else if (check(buffer[i], buffer[j], sortReverse)) {
+                    a[m] = buffer[j];
+                    j++;
             } else {
-                a[m] = buf[i];
+                a[m] = buffer[i];
                 i++;
             }
         }
     }
-}
 
+    /**
+     * func for checking sortReverse mode and compare elements
+     * @param i first element for compare
+     * @param j second element for compare
+     * @param sortReverse sortReverse mode
+     * @return result of compare
+     */
+    private static boolean check(int i, int j, boolean sortReverse){
+
+        if(!sortReverse)
+            return (j < i);
+        else
+            return (i < j);
+    }
+}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,32 +2,39 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
     //@Ignore
-    public void sortTest() {
+    public void sortSingleArrayTest() {
 
         int[] input = new int[]{11, 2, 23, 14, 35, 16, 47, 18};
         int[] result = new int[]{2, 11, 14, 16, 18, 23, 35, 47};
 
-        System.out.println("input array: " + input.toString());
+        System.out.println("TEST : Sort test:");
+        System.out.println("\tinput array: " + Arrays.toString(input));
 
         Assert.assertArrayEquals(Main.lounchSort(input,false),result);
+
+        System.out.println("\toutput array: " + Arrays.toString(input));
     }
 
-//    @Test
-//    @Ignore
-//    public void sortReverseTest() {
-//          String[] = ("-d", "out.")
-//        Main.main(String[])
-//        //TODO: correct array of result to match sorted array
-//        int[] result = new int[]{47, 11, 14, 16, 18, 23, 35, 2};
-//
-//        System.out.println("input array: " + input.toString());
-//
-//        Assert.assertArrayEquals(Main.sort(input,'a'),result);
-//    }
+    @Test
+    //@Ignore
+    public void sortSingleArrayReverseTest() {
+
+    int[] input = new int[]{11, 2, 23, 14, 35, 16, 47, 18};
+    int[] result = new int[]{47, 35, 23, 18, 16, 14, 11, 2};
+
+        System.out.println("TEST : Sort reverse test:");
+        System.out.println("\tinput array: " + Arrays.toString(input));
+
+        Assert.assertArrayEquals(Main.lounchSort(input,true),result);
+
+        System.out.println("\toutput array: " + Arrays.toString(input));
+    }
 }
